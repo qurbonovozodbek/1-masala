ca3e13a9ca600a6558de424a9d7d54302e3e5b72
qurbonovozodbek <ozodbekkurbonov185@gmail.com>
2023-12-07 18:05:01 +0500

    1-masala dobe

---
+++ index.js
@@ -0,0 +1,35 @@
+// BIRINCHI MASALA
+
+// let BirinchiSon = +prompt(`Birinchi sonni kiriting!`);
+// let IkkinchiSon = +prompt(`Ikkinchi sonni kiriting!`);
+// let UchinchiSon = +prompt(`Uchinchi sonni kiriting!`);
+// alert(BirinchiSon+IkkinchiSon+UchinchiSon);
+// console.log(BirinchiSon+IkkinchiSon+UchinchiSon);
+
+
+// IKKINCHI MASALA
+
+// let BirinchiSon = +prompt(`Birinchi sonni kiriting`);
+// let IkkinchiSon = +prompt(`Ikkinchi sonni kiriting`);
+// alert((BirinchiSon*BirinchiSon)+(IkkinchiSon*IkkinchiSon));
+// console.log((BirinchiSon*BirinchiSon)+(IkkinchiSon*IkkinchiSon));
+
+
+// UCHINCHI MASALA
+
+// let BirinchiSon = +prompt(`Birinchi sonni kiriting!`);
+// let IkkinchiSon = +prompt(`Ikkinchi sonni kiriting!`);
+// alert((BirinchiSon+IkkinchiSon)*2)
+// console.log((BirinchiSon+IkkinchiSon)*2)
+
+// TO'RTINCHI MASALA
+
+// let BirinchiSon = +prompt(`Kubning tomonini kiriting!`);
+// alert((BirinchiSon*2)*6);
+// console.log(`(BirinchiSon*2)*6`);
+
+// BESHINCHI MASALA
+
+// let BirinchiSon = +prompt(`Doiraning diametrini kiriting`);
+// alert((BirinchiSon*2)*3.14);
+// console.log(`(BirinchiSon*2)*3.14`);
